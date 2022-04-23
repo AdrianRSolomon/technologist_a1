414b54ca4783fce22b85fed41937700a1ac9a4aa
ADRIAN R SOLOMON <70720689+AdrianPrinceSolomon@users.noreply.github.com>
2022-04-22 20:37:39 -0400

    Added my project

--- components/navbar.js
+++ components/navbar.js
@@ -34,18 +34,18 @@ const Navbar = () => {
                           </ul>
                       </nav>
                   </header>
-                  <section class="text-gray-600 body-font">
-                    <div class="container mx-auto flex px-5 py-16 md:flex-row flex-col items-center">
-                        <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-                        <img class="object-cover object-center rounded" alt="hero" src="/img-2.png"s/>
+                  <section className="text-gray-600 body-font">
+                    <div className="container mx-auto flex px-5 py-16 md:flex-row flex-col items-center">
+                        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+                        <img className="object-cover object-center rounded" alt="hero" src="/img-2.png"s/>
                         </div>
-                        <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+                        <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
                         <h1 className="text-white font-semibold text-2xl xl:text-2xl">
                         Quality repairing at your Affordability at <br></br>
                             <span className="text-[#7af4fc] text-4xl xl:text-4xl"> A1 Technologist</span>
                         </h1>
-                        <p class="mb-8 leading-relaxed text-gray-50">If you are looking for computer repair services, then choosing is an ideal solution for you.</p>
-                        <div class="flex justify-center">
+                        <p className="mb-8 leading-relaxed text-gray-50">If you are looking for computer repair services, then choosing is an ideal solution for you.</p>
+                        <div className="flex justify-center">
                         <a href="javascript:void(0)" className="px-5 py-1 w-full bg-white text-gray-800 hover:bg-[#7af4fc] text-center rounded-md shadow-md block sm:w-auto">
                                   Book Appointment
                               </a>
